3603935dde8f2a6b919cdce52b0c0981734ea755
Vince Aggrippino <yhqktpi@gmail.com>
2017-02-22 01:05:21 +0000

    Add the basic item use case to spec.
    Remove the redundant Item Struct, comment the placeholder contexts, and add a
    context for the basic item use case.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,35 +1,41 @@
 require_relative '../lib/gilded_rose'
 
-Item = Struct.new(:name, :sell_in, :quality)
 item = Item.new('basic item', 5, 10)
 
 describe "#update_quality" do
-
-  context "with a single item" do
-    let(:initial_sell_in) { 5 }
-    let(:initial_quality) { 10 }
-    let(:name) { "item" }
-    let(:item) { Item.new(name, initial_sell_in, initial_quality) }
-
+  context "Given a basic item" do
     before { update_quality([item]) }
 
-    it "your specs here" do
-      pending
+    it "reduces the item's sell_in and quality values" do
+      expect(item).to have_attributes(:sell_in => 4, :quality => 9)
     end
   end
 
-  context "with multiple items" do
-    let(:items) {
-      [
-        Item.new("NORMAL ITEM", 5, 10),
-        Item.new("Aged Brie", 3, 10),
-      ]
-    }
+  # context "with a single item" do
+  #   let(:initial_sell_in) { 5 }
+  #   let(:initial_quality) { 10 }
+  #   let(:name) { "item" }
+  #   let(:item) { Item.new(name, initial_sell_in, initial_quality) }
 
-    before { update_quality(items) }
+  #   before { update_quality([item]) }
 
-    it "your specs here" do
-      pending
-    end
-  end
+  #   it "your specs here" do
+  #     pending
+  #   end
+  # end
+
+  # context "with multiple items" do
+  #   let(:items) {
+  #     [
+  #       Item.new("NORMAL ITEM", 5, 10),
+  #       Item.new("Aged Brie", 3, 10),
+  #     ]
+  #   }
+
+  #   before { update_quality(items) }
+
+  #   it "your specs here" do
+  #     pending
+  #   end
+  # end
 end
